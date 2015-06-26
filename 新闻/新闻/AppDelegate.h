4666b56380a89f10928a993e17ae1ea733640b5a
郭振虎 <guozhenhu@guozhenhu.local>
2015-06-26 14:55:22 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by 郭振虎 on 15/6/26.
+//  Copyright (c) 2015年 郭振虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
