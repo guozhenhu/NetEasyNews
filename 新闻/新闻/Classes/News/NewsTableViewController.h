d3ec019caebd4c9c2121e7bbfabf320b60d9c73e
郭振虎 <guozhenhu@guozhenhu.local>
2015-06-26 15:10:47 +0800

    搭建项目框架

--- 新闻/新闻/Classes/News/NewsTableViewController.h
+++ 新闻/新闻/Classes/News/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  新闻
 //
 //  Created by 郭振虎 on 15/6/26.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
